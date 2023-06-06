6cd5624d9b847ae53843f78989ef5827d41754e3
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-06-06 16:41:53 +0200

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,24 @@
+﻿using CitizenFX.Core;
+using CitizenFX.FiveM;
+using System.Dynamic;
+using System;
+using CitizenFX.FiveM.Native;
+
+namespace c_zones_influence
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+            EventHandlers["onClientResourceStart"] += Func.Create<string>(OnClientResourceStart);
+
+        }
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (resourceName != Natives.GetCurrentResourceName())
+            {
+                return;
+            }
+        }
+    }
+}
