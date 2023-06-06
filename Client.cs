7bbe93241c2edcd340f19796a95231e011fdcb5f
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-06-06 18:04:00 +0200

    add simple c# kod and mathematical mapping system in js

--- Client.cs
+++ Client.cs
@@ -19,6 +19,22 @@ namespace c_zones_influence
             {
                 return;
             }
+
+            Natives.RegisterNuiCallbackType("c_influence");
+            EventHandlers["__cfx_nui:c_influence"] += Func.Create<ExpandoObject>(influence);
+
+            Natives.RegisterCommand("test", new Action(test), false);
+        }
+
+        private void test() 
+        {
+            Vector3 coords = Natives.GetEntityCoords(Natives.PlayerPedId(), false);
+            Natives.SendNuiMessage("{\"action\":\"start\",\"x\":"+ coords[0].ToString() + ", \"y\":" + coords[1].ToString() + "}");
+        }
+
+        private void influence(dynamic data) 
+        {
+            Debug.WriteLine(data.ToString());   
         }
     }
 }
