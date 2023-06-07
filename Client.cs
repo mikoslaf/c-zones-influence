4648d50afbcda21c59da20eb887a7a96331fa551
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-06-07 11:43:05 +0200

    add setgang function cooperating with qb-framework and a lot of other thinks

--- Client.cs
+++ Client.cs
@@ -8,6 +8,7 @@ namespace c_zones_influence
 {
     public class Client : BaseScript
     {
+        private string gang = "none";
         public Client()
         {
             EventHandlers["onClientResourceStart"] += Func.Create<string>(OnClientResourceStart);
@@ -20,13 +21,26 @@ namespace c_zones_influence
                 return;
             }
 
+            Events.RegisterEventHandler("QBCore:Client:OnGangUpdate", Func.Create<ExpandoObject>(setgang), Binding.All);
+            //setgang("Vagos"); //test
+
             Natives.RegisterNuiCallbackType("c_influence");
             EventHandlers["__cfx_nui:c_influence"] += Func.Create<ExpandoObject>(influence);
+            EventHandlers["QBCore:Client:OnGangUpdate"] += Func.Create<string>(setgang);
 
             Natives.RegisterCommand("test", new Action(test), false);
         }
 
-        private void test() 
+        private void setgang(dynamic data)
+        {
+            gang = data.name;
+            if (gang != "none")
+            {
+                //do something
+            }
+        }
+
+        private void test()
         {
             Vector3 coords = Natives.GetEntityCoords(Natives.PlayerPedId(), false);
             Natives.SendNuiMessage("{\"action\":\"start\",\"x\":"+ coords[0].ToString() + ", \"y\":" + coords[1].ToString() + "}");
@@ -34,7 +48,13 @@ namespace c_zones_influence
 
         private void influence(dynamic data) 
         {
-            Debug.WriteLine(data.ToString());   
+            if (gang != "none")
+            {
+                //byte zone = Convert.ToByte(data.zone);
+                uint zone = data.zone;
+                double val = data.val;
+                Events.TriggerServerEvent("qb-handler:c-zones-influence:influence",zone,val,gang);
+            }
         }
     }
 }
