5b9708f5594bb68e01f2f0ab90ef1a97cba3bc9c
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-06-11 12:55:59 +0200

    add influence funtion in server site

--- Server/Server.cs
+++ Server/Server.cs
@@ -8,7 +8,7 @@ namespace Server
     public class Zone
     {
         public string gang { get; set; }
-        public float val { get; set; }
+        public double val { get; set; }
     }
     public class Server: BaseScript
     {
@@ -28,8 +28,10 @@ namespace Server
         {
             foreach (var item in data)
             {
-                Zones.Add(Convert.ToInt32(item.Key), new Zone { gang = item.Value.gang, val = (float)item.Value.val });
+                Zones.Add(Convert.ToInt32(item.Key), new Zone { gang = item.Value.gang, val = item.Value.val });
             }
+
+            influence(1, 0.01, "vagos");
         }
         private void addplayer([Source] Player source, string gang) 
         {
@@ -45,7 +47,7 @@ namespace Server
 
             foreach (KeyValuePair<int, string> v in influencers)
             {
-                Console.WriteLine("Key = {0}, Value = {1}", v.Key, v.Value);
+                Debug.WriteLine("Key = {0}, Value = {1}", v.Key, v.Value);
             }
             Debug.WriteLine($"Player {source.Handle}");
         }
@@ -58,7 +60,48 @@ namespace Server
                 {
                     Debug.Write("alarm player {0}", v.Key);
                 }
-                Console.WriteLine("Key = {0}, Value = {1}", v.Key, v.Value);
+                Debug.WriteLine("Key = {0}, Value = {1}", v.Key, v.Value);
+            }
+        }
+
+        private void influence(int id, double val, string gang) 
+        {
+            if (Zones[id].gang == gang)
+            {
+                if (Zones[id].val != 1)
+                {
+                    double add = Zones[id].val + val;
+                    if (add > 1)
+                    {
+                        Zones[id].val = 1;
+                    }
+                    else
+                    {
+                        Zones[id].val = add;
+                    }
+
+                    Events.TriggerEvent("qb-handler:c-zones-influence:influence", id, Zones[id].val);
+                }
+            }
+            else
+            {
+                double remove = Zones[id].val - val;
+                if (remove <= 0)
+                {
+                    Zones[id].val = 0.01;
+                    Zones[id].gang = gang;
+                    Events.TriggerEvent("qb-handler:c-zones-influence:influence", id, Zones[id].val, gang);
+                }
+                else
+                {
+                    Zones[id].val = remove;
+                    Events.TriggerEvent("qb-handler:c-zones-influence:influence", id, Zones[id].val);
+                }
+            }
+
+            foreach (KeyValuePair<int, Zone> v in Zones)
+            {
+                Debug.WriteLine("Key = {0}, Value = {1}", v.Value.gang, v.Value.val);
             }
         }
 
