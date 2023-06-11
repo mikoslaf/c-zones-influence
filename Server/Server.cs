6d02d32894bd274f7a4d7bfb3e7e541cc8bac665
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-06-11 14:40:42 +0200

    rebuild addplayer function

--- Server/Server.cs
+++ Server/Server.cs
@@ -2,6 +2,7 @@
 using CitizenFX.Server;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Server
 {
@@ -12,14 +13,14 @@ namespace Server
     }
     public class Server: BaseScript
     {
-        private Dictionary<int, string> influencers = new Dictionary<int, string>();
+        private Dictionary<string, List<int>> influencers = new Dictionary<string, List<int>>();
         private Dictionary<int, Zone> Zones = new Dictionary<int, Zone>();
         public Server() 
         {
             Debug.WriteLine("1234");
 
             //EventHandlers["chatMessage"] += Func.Create<int, string, string>(addplayer);
-            Events.RegisterEventHandler("c-zones-influence:player", Func.Create<Player, string>(addplayer), Binding.All);
+            Events.RegisterEventHandler("c-zones-influence:addplayer", Func.Create<Player, string>(addplayer), Binding.All);
             Events.RegisterEventHandler("c-zones-influence:alarmplayer", Func.Create<string, string>(alarmplayer), Binding.All);
             Events.RegisterEventHandler("c-zones-influence:setzones", Func.Create<dynamic>(setzones), Binding.All);
         }
@@ -29,39 +30,66 @@ namespace Server
             foreach (var item in data)
             {
                 Zones.Add(Convert.ToInt32(item.Key), new Zone { gang = item.Value.gang, val = item.Value.val });
-            }
-
-            influence(1, 0.01, "vagos");
+            }   
         }
         private void addplayer([Source] Player source, string gang) 
         {
-            Debug.WriteLine(gang);
             if (gang == "none")
             {
-                influencers.Remove(source.Handle);
+                foreach (var v in influencers.Values)
+                {
+                    if (v.Remove(source.Handle)) 
+                    {
+                        if (v.Count == 0) 
+                        {
+                            foreach (KeyValuePair<string, List<int>> i in influencers)
+                            {
+                                if (!i.Value.Any()) 
+                                {
+                                    influencers.Remove(i.Key);
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
             }
             else 
             {
-                influencers.Add(source.Handle, gang);
+                if (influencers.ContainsKey(gang))
+                {
+                    if (!influencers[gang].Exists(x => x == source.Handle)) 
+                    {
+                        influencers[gang].Add(source.Handle);
+                    }
+                }
+                else 
+                {
+                    influencers.Add(gang,new List<int> { source.Handle });
+                }
             }
 
-            foreach (KeyValuePair<int, string> v in influencers)
+            foreach (KeyValuePair<string, List<int>> v in influencers)
             {
                 Debug.WriteLine("Key = {0}, Value = {1}", v.Key, v.Value);
+                foreach (var item in v.Value) 
+                {
+                    Debug.Write(item.ToString() + " ");
+                }
             }
-            Debug.WriteLine($"Player {source.Handle}");
         }
 
         private void alarmplayer(string gang, string note) 
         {
-            foreach (KeyValuePair<int, string> v in influencers)
-            {
-                if (v.Value == gang) 
-                {
-                    Debug.Write("alarm player {0}", v.Key);
-                }
-                Debug.WriteLine("Key = {0}, Value = {1}", v.Key, v.Value);
-            }
+            //foreach (KeyValuePair<int, string> v in influencers)
+            //{
+            //    if (v.Value == gang) 
+            //    {
+            //        Debug.Write("alarm player {0}", v.Key);
+            //    }
+            //    Debug.WriteLine("Key = {0}, Value = {1}", v.Key, v.Value);
+            //}
         }
 
         private void influence(int id, double val, string gang) 
